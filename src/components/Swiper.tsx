5580cb9b963f5f04eb5475d65471b533bcee0bc1
Bombergio <cosmaori@gmail.com>
2022-10-10 22:00:03 +0200

    Minor cleanup

--- src/components/Swiper.tsx
+++ src/components/Swiper.tsx
@@ -1,7 +1,7 @@
-import LightGallery from 'lightgallery/react/Lightgallery.umd.js';
-import 'lightgallery/css/lightgallery.css';
-import { useCallback } from 'react';
-import type { Photo } from './common/Types';
+import LightGallery from 'lightgallery/react/Lightgallery.umd.js'
+import 'lightgallery/css/lightgallery.css'
+import { useCallback } from 'react'
+import type { Photo } from './common/Types'
 
 interface Props {
   photos: Photo[]
@@ -10,16 +10,15 @@ interface Props {
 
 export function Swiper ({ photos, licenseKey}:Props) {
   function renderClass(i: number) {
-    if (i === 0) {return 'object-cover w-full rounded shadow-lg h-56 col-span-2 cursor-pointer duration-500 hover:scale-105'} else
-    if (i < 3) {return 'object-cover w-full rounded shadow-lg h-48 cursor-pointer duration-500 hover:scale-105'} else 
+    if (i === 0) {return 'object-cover w-full rounded shadow-lg h-56 col-span-2 cursor-pointer duration-500 hover:scale-105 lazy'} else
+    if (i < 3) {return 'object-cover w-full rounded shadow-lg h-48 cursor-pointer duration-500 hover:scale-105 lazy'} else 
     return 'hidden'
   }
-
 
   const getItems = useCallback(() => {
     return photos.map((item, index) => {
         return (
-          <img className={renderClass(index)} src={index < 3 ? item.thumbnails.large.url : ""} alt={item.filename} data-src={item.url} key={item.id}/>
+          <img className={renderClass(index)} src={index < 3 ? item.thumbnails.large.url : ""} alt={item.filename} data-src={item.url} key={item.id} loading="lazy"/>
         );
     });
   }, [photos]);
