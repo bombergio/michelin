6550d79455ad7837fbb925e86472a1c265d5f36a
Bombergio <cosmaori@gmail.com>
2022-12-04 13:01:00 +0100

    Updated thumbnails

--- src/components/Swiper.tsx
+++ src/components/Swiper.tsx
@@ -10,17 +10,17 @@ interface Props {
 export function Swiper ({ photos}:Props) {
   const { PUBLIC_LIGHTGALLERY_LICENSE_KEY } = import.meta.env
 
-  function renderClass(i: number) {
-    if (i === 0) {return 'object-cover w-full rounded shadow-lg h-56 col-span-2 cursor-pointer duration-500 hover:scale-105 lazy'} else
-    if (i < 3) {return 'object-cover w-full rounded shadow-lg h-48 cursor-pointer duration-500 hover:scale-105 lazy'} else 
-    return 'hidden'
+  function img_attr(i: number): { class: string; tr: string } {
+    if (i === 0) {return { class: 'object-cover w-full rounded shadow-lg h-56 col-span-2 cursor-pointer duration-500 hover:scale-105 lazy', tr: 'tr:w-0.99,h-400/' }} else
+    if (i < 3) {return { class: 'object-cover w-full rounded shadow-lg h-48 cursor-pointer duration-500 hover:scale-105 lazy', tr: 'tr:w-400,h-400/'}} else 
+    return { class: 'hidden', tr: '' }
   }
 
   const getItems = useCallback(() => {
     return photos.map((item, index) => {
       const filename=item.filename.replaceAll(" ", "_")
       return (
-        <img className={renderClass(index)} src={index < 3 ? "https://ik.imagekit.io/bomberg/tr:n-large_thumb/" + filename : ""} alt={filename} data-src={"https://ik.imagekit.io/bomberg/" + filename} key={item.id} loading="lazy"/>
+        <img className={img_attr(index).class} src={index < 3 ? "https://ik.imagekit.io/bomberg/" + img_attr(index).tr + filename : ""} alt={filename} data-src={"https://ik.imagekit.io/bomberg/" + filename} key={item.id} loading="lazy"/>
       );
     });
   }, [photos]);
