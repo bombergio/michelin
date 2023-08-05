111c4f3f05f24ca441bcb4f744bf622eb2aaa981
Bombergio <cosmaori@gmail.com>
2023-08-05 22:53:11 +0200

    Replaced Airtable API key with PAT

--- utils/airtable.ts
+++ utils/airtable.ts
@@ -1,10 +1,9 @@
 export async function listPlaces(visited: Boolean = true): Promise<any> {
   const url = 'https://api.airtable.com/v0/appUtaNDGxbz3kGSO/michelin_restaurants'
-  
-  const { AIRTABLE_API_KEY } = import.meta.env
+
+  const { AIRTABLE_PAT } = import.meta.env
   const params = {
     maxRecords: 100,
-    api_key: AIRTABLE_API_KEY,
     view: 'Grid view',
     filterByFormula: visited ? 'Visited' : 'NOT(Visited)'
   }
@@ -12,7 +11,7 @@ export async function listPlaces(visited: Boolean = true): Promise<any> {
   const queryString = Object.keys(params).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(params[key])).join('&')
   const airtableUrl = url + '?' + queryString
 
-  const response = await fetch(airtableUrl)
+  const response = await fetch(airtableUrl, { headers: { Authorization: `Bearer ${AIRTABLE_PAT}` }})
   const data = await response.json()
   const places = data ? data.records : null
 
