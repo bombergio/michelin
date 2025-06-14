937fea9299c5c6021dda36441ff1579ac2f210a5
Bombergio <cosmaori@gmail.com>
2025-06-14 19:26:51 +0200

    Updated Tailwindcss to version 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: { applyBaseStyles: true },
-  }), react()]
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
